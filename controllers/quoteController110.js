354ee0ed9c7b02b0bf739b7ec39148b42432bfb6
Baptiste Hardelin <91209529+BaptisteHardelin@users.noreply.github.com>
2023-02-15 22:32:11 +0100

    Add a way to get a single author by id

--- controllers/quoteController110.js
+++ controllers/quoteController110.js
@@ -11,13 +11,24 @@ db.exec("PRAGMA foreign_keys = ON");
 const getAllAuthors = (req, res) => {
   db.all("select * from author", (err, rows) => {
     if (err) {
-      return res.status(404).json(err);
+      return res.status(500).json(err);
     }
 
     res.json({ authors: rows }).status(200);
   });
 };
 
+const getSingleAuthor = (req, res) => {
+  const id = req.params.id;
+  db.get("select * from author where auth_id=?", id, (err, row) => {
+    if (err != null) {
+      res.status(204);
+    }
+    res.json({ author: row }).status(200);
+  });
+};
+
 module.exports = {
   getAllAuthors,
+  getSingleAuthor,
 };
--- routes/routes110.js
+++ routes/routes110.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const routes = express.Router();
-const { getAllAuthors } = require("../controllers/quoteController110");
+const {
+  getAllAuthors,
+  getSingleAuthor,
+} = require("../controllers/quoteController110");
 
 routes.get("/", (req, res) => {
   res.json("Hello, World! 110");
 });
 
 routes.get("/authors", getAllAuthors);
+routes.get("/authors/:id", getSingleAuthor);
 
 module.exports = routes;
